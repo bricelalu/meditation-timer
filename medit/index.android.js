8b93b441248b7366fd066ee0f979dccad025e2a4
Brice LALU <brice@.quable.fr>
2017-05-17 22:48:46 +0200

    Add ReactNativeSound to play sound

--- medit/index.android.js
+++ medit/index.android.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, Alert, Button, StyleSheet,
-TouchableOpacity } from 'react-native';
+import {
+  AppRegistry,
+  TimePickerAndroid,
+  Text,
+  View,
+  Alert,
+  Button,
+  StyleSheet,
+  TouchableOpacity } from 'react-native';
 import Video from 'react-native-video';
 import renderIf from './src/utils/renderIf';
+
+const Sound = require('react-native-sound');
+
+const gongSound = new Sound('gong.mp3', Sound.MAIN_BUNDLE, (error) => {
+  if (error) {
+    console.log('failed to load the sound', error);
+    return;
+  }
+  // loaded successfully
+  console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+});
 
 class Greeting extends Component {
   render() {
@@ -17,10 +35,20 @@ class HelloWorldApp extends Component {
     super(props);
     this.onButtonPress = this.onButtonPress.bind(this);
     this.onPlayGongButtonPressed = this.onPlayGongButtonPressed.bind(this);
+    this.getDurationTitle = this.getDurationTitle.bind(this);
     this.state = {
       isFirstButtonPressed: false,
       isPlayGongButtonPressed: false,
+      presetHour: 0,
+      presetMinute: 5,
+      totalDurationMinute: 5
     };
+  }
+  onMeditLengthChoose() {
+    Alert.alert('MeditLengthChoose');
+  }
+  onMeditStart() {
+    Alert.alert('MeditStart');
   }
   onButtonPress() {
     if (this.state.isFirstButtonPressed) {
@@ -28,7 +56,13 @@ class HelloWorldApp extends Component {
     } else {
       this.setState({ isFirstButtonPressed: true });
     }
-
+    gongSound.play((success) => {
+      if (success) {
+        console.log('successfully finished playing');
+      } else {
+        console.log('playback failed due to audio decoding errors');
+      }
+    });
     Alert.alert('Button has been pressed!');
   }
   onPlayGongButtonPressed() {
@@ -46,12 +80,62 @@ class HelloWorldApp extends Component {
 
     Alert.alert('You should hear the gong!');
   }
+  getDurationTitle() {
+      return `Durée : ${this.totalDurationMinute} min `;
+  }
+  showPicker = async (stateKey, options) => {
+    try {
+      const { action, minute, hour } = await TimePickerAndroid.open(options);
+      var newState = {};
+      if (action === TimePickerAndroid.timeSetAction) {
+        newState[stateKey + 'Text'] = formatTime(hour, minute);
+        newState[stateKey + 'Hour'] = hour;
+        newState[stateKey + 'Minute'] = minute;
+      } else if (action === TimePickerAndroid.dismissedAction) {
+        newState[stateKey + 'Text'] = 'dismissed';
+      }
+      console.warn(newState);
+      this.setState(newState);
+    } catch ({ code, message }) {
+      console.warn(`Error in example '${stateKey}': `, message);
+    }
+  }
   render() {
     return (
       <View style={{ alignItems: 'center' }}>
         <Greeting name='Rexxar' />
         <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
+          <Button
+          onPress={this.showPicker.bind(this, 'totalDuration', {
+            hour: this.state.presetHour,
+            minute: this.state.presetMinute,
+            is24Hour: true,
+          })}
+          title="Durée : 5 minutes"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+          />
+          <Button
+          onPress={this.showPicker.bind(this, 'intervalDuration', {
+            hour: this.state.presetHour,
+            minute: this.state.presetMinute,
+            is24Hour: true,
+          })}
+          title="Durée d'intervalle"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+          />
+        <Text>{this.state.totalDurationText} text</Text>
+        <Text>{this.state.totalDurationHour} hour</Text>
+        <Text>{this.state.totalDurationMinute} minutes</Text>
+        <Text>Interval: {this.state.intervalDurationMinute} minutes</Text>
+        <Button
+    onPress={this.onMeditStart}
+    title="Lancer"
+    color="#841584"
+    accessibilityLabel="Learn more about this purple button"
+        />
+      <Greeting name='Valeera' />
           <Button
     onPress={this.onButtonPress}
     title="Learn More"
@@ -71,214 +155,10 @@ class HelloWorldApp extends Component {
   }
 }
 
-class VideoPlayer extends Component {
-
-  state = {
-    rate: 1,
-    volume: 1,
-    muted: false,
-    resizeMode: 'contain',
-    duration: 0.0,
-    currentTime: 0.0,
-    paused: true,
-  };
-
-  onLoad = (data) => {
-    this.setState({ duration: data.duration });
-  };
-
-  onProgress = (data) => {
-    this.setState({ currentTime: data.currentTime });
-  };
-
-  onEnd = () => {
-    this.setState({ paused: true });
-    this.video.seek(0);
-  };
-
-  onAudioBecomingNoisy = () => {
-    this.setState({ paused: true });
-  }
-
-  onAudioFocusChanged = (event: { hasAudioFocus: boolean }) => {
-    this.setState({ paused: !event.hasAudioFocus });
-  }
-
-  getCurrentTimePercentage() {
-    if (this.state.currentTime > 0) {
-      return parseFloat(this.state.currentTime) / parseFloat(this.state.duration);
-    }
-    return 0;
-  }
-  video: Video;
-
-  renderRateControl(rate) {
-    const isSelected = (this.state.rate === rate);
-
-    return (
-      <TouchableOpacity onPress={() => { this.setState({ rate }); }}>
-        <Text style={[styles.controlOption, { fontWeight: isSelected ? 'bold' : 'normal' }]}>
-          {rate}x
-        </Text>
-      </TouchableOpacity>
-    );
-  }
-
-  renderResizeModeControl(resizeMode) {
-    const isSelected = (this.state.resizeMode === resizeMode);
-
-    return (
-      <TouchableOpacity onPress={() => { this.setState({ resizeMode }); }}>
-        <Text style={[styles.controlOption, { fontWeight: isSelected ? 'bold' : 'normal' }]}>
-          {resizeMode}
-        </Text>
-      </TouchableOpacity>
-    );
-  }
-
-  renderVolumeControl(volume) {
-    const isSelected = (this.state.volume === volume);
-
-    return (
-      <TouchableOpacity onPress={() => { this.setState({ volume }); }}>
-        <Text style={[styles.controlOption, { fontWeight: isSelected ? 'bold' : 'normal' }]}>
-          {volume * 100}%
-        </Text>
-      </TouchableOpacity>
-    );
-  }
-
-  render() {
-    const flexCompleted = this.getCurrentTimePercentage() * 100;
-    const flexRemaining = (1 - this.getCurrentTimePercentage()) * 100;
-
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.fullScreen}
-          onPress={() => this.setState({ paused: !this.state.paused })}
-        >
-          <Video
-            ref={(ref: Video) => { this.video = ref; }}
-            source={require('./src/components/gong.mp3')}
-            style={styles.fullScreen}
-            rate={this.state.rate}
-            paused={this.state.paused}
-            volume={this.state.volume}
-            muted={this.state.muted}
-            resizeMode={this.state.resizeMode}
-            onLoad={this.onLoad}
-            onProgress={this.onProgress}
-            onEnd={this.onEnd}
-            onAudioBecomingNoisy={this.onAudioBecomingNoisy}
-            onAudioFocusChanged={this.onAudioFocusChanged}
-            repeat={false}
-          />
-        </TouchableOpacity>
-
-        <View style={styles.controls}>
-          <View style={styles.generalControls}>
-            <View style={styles.rateControl}>
-              {this.renderRateControl(0.25)}
-              {this.renderRateControl(0.5)}
-              {this.renderRateControl(1.0)}
-              {this.renderRateControl(1.5)}
-              {this.renderRateControl(2.0)}
-            </View>
-
-            <View style={styles.volumeControl}>
-              {this.renderVolumeControl(0.5)}
-              {this.renderVolumeControl(1)}
-              {this.renderVolumeControl(1.5)}
-            </View>
-
-            <View style={styles.resizeModeControl}>
-              {this.renderResizeModeControl('cover')}
-              {this.renderResizeModeControl('contain')}
-              {this.renderResizeModeControl('stretch')}
-            </View>
-          </View>
-
-          <View style={styles.trackingControls}>
-            <View style={styles.progress}>
-              <View style={[styles.innerProgressCompleted, { flex: flexCompleted }]} />
-              <View style={[styles.innerProgressRemaining, { flex: flexRemaining }]} />
-            </View>
-          </View>
-        </View>
-      </View>
-    );
-  }
+/**
+ * Returns e.g. '3:05'.
+ */
+function formatTime(hour, minute) {
+  return hour + ':' + (minute < 10 ? '0' + minute : minute);
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black',
-  },
-  fullScreen: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
-  controls: {
-    backgroundColor: 'transparent',
-    borderRadius: 5,
-    position: 'absolute',
-    bottom: 20,
-    left: 20,
-    right: 20,
-  },
-  progress: {
-    flex: 1,
-    flexDirection: 'row',
-    borderRadius: 3,
-    overflow: 'hidden',
-  },
-  innerProgressCompleted: {
-    height: 20,
-    backgroundColor: '#cccccc',
-  },
-  innerProgressRemaining: {
-    height: 20,
-    backgroundColor: '#2C2C2C',
-  },
-  generalControls: {
-    flex: 1,
-    flexDirection: 'row',
-    borderRadius: 4,
-    overflow: 'hidden',
-    paddingBottom: 10,
-  },
-  rateControl: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  volumeControl: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  resizeModeControl: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  controlOption: {
-    alignSelf: 'center',
-    fontSize: 11,
-    color: 'white',
-    paddingLeft: 2,
-    paddingRight: 2,
-    lineHeight: 12,
-  },
-});
-
 AppRegistry.registerComponent('medit', () => HelloWorldApp);
