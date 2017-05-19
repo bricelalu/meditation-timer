bd25fd4fc5d630594b39a2785da8bdb0e7d93953
Brice LALU <brice@.quable.fr>
2017-05-19 13:33:40 +0200

    add missing header component

---
+++ medit/src/components/header.js
@@ -0,0 +1,38 @@
+// Import libraries for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Make a component
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#808080',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 40,
+    width: '100%',
+    paddingTop: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20,
+    color: '#fffafa',
+    fontWeight: 'bold'
+  }
+};
+
+// Make the component available to other parts of the app
+export default Header;
